5cedc9d441c96bbcd616e50f2a2bdfdae346bddf
Steven Andrews <steven.s.andrews@gmail.com>
2019-02-22 15:56:59 -0800

    added examples directory

---
+++ examples/S2_config/clusterscripts/smlaunch.sh
@@ -0,0 +1,16 @@
+## This is a shell script for running Smoldyn on a cluster. Replace 'mydir' with your directory name.
+
+## declare a variable: name of the Smoldyn configuration file; needs to start with a letter
+declare cnum="c1"
+
+## declare an array variable: e.g. readfiles with different molecule starting positions
+declare -a arr=("sphere" "triangle" "cube")
+
+## loop through the above array; loop through 
+for i in "${arr[@]}"; do
+	for j in {1..30}; do
+	sname="$cnum$i$j"
+	qsub -V -cwd -o /mydir/"$cnum"/logs/ -e /mydir/"$cnum"/logs/ -N $sname -b y smoldyn /mydir/"$cnum"/"$cnum"_run.txt --define SIMNAME=$i --define SIMNUMBER=$j;
+	echo $i,$j;
+done
+done
---
+++ examples/S95_regression/regression.py
@@ -0,0 +1,48 @@
+# Regression testing for Smoldyn
+import os
+import filecmp
+import sys
+
+filelist=['addspecies','abba','allostery','bimolecularlattice','bounce2','bounds1','bounds2','branch',
+					'cmdmanipulate','compartlogic','compartrxn','crowding',
+					'diffa','diffuse2b','diffuse2c','diffuse3b','drift','dumbbell',
+					'emitter1','hemisphere','lrm','lrmsim','lotvolt','molecule',
+					'polymer_end','polymer_mid','rafts','rafts3D',
+					'structexpansion','surf1','surf2','surf3','surfaceboundjump','surfacedrift2',
+					'surfacedrift3','surfacereact','stick',
+					'surfacediffuse','tracking','trisphere']
+
+suffix=input('Enter suffix for new simulations: ')
+reference=input('Enter suffix for reference simulations or "none": ')
+
+if reference!='none':
+	dirname='output_%s/' %(reference)
+	if not os.path.exists(dirname):
+		print('reference directory "%s" does not exist' %(reference))
+		sys.exit()
+
+dirname='output_%s' %(suffix)
+try:
+	os.makedirs(dirname)
+except OSError:
+	if os.path.exists(dirname):
+		pass
+	else:
+		raise
+
+for filename in filelist:
+	filenew='output_'+suffix+'/'+filename+'_'+suffix+'_out.txt'
+	fileref='output_'+reference+'/'+filename+'_'+reference+'_out.txt'
+	string='smoldyn %s.txt --define OUTFILE=%s -tqw' %(filename,filenew)
+	#	print('system call: %s' %(string))
+	os.system(string)
+	if reference=='none':
+		print('finished %s.txt' %(filename))
+	elif not os.path.exists(fileref):
+		print('added: %s.txt' %(filename))
+	else:
+		same=filecmp.cmp(filenew,fileref)
+		if same:
+			print('same: %s.txt' %(filename))
+		else:
+			print('DIFFERENT: %s.txt' %(filename))
