21e0e16de49a057a5ff24f896e2d21a852fa09b2
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-19 03:53:42 +0530

    Generate bindings as well.

--- source/py/pysmoldyn.cpp
+++ source/py/pysmoldyn.cpp
@@ -3,16 +3,25 @@
  * Author: Dilawar Singh <dilawar.s.rajput@gmail.com>
  */
 
+#include <string>
+using namespace std;
+
 #include <pybind11/pybind11.h>
 
 #include "../Smoldyn/smoldynfuncs.h"
 
 namespace py = pybind11;
 
-int simulate(const char* filename, const char* fileroot, const char* flags) 
+int simulate(const string& filepath, const string& flags) 
 {
+    string filename, fileroot;
+    auto pos = filepath.find_last_of('/');
+
+    fileroot = filepath.substr(0, pos);
+    filename = filepath.substr(pos+1);
+
     simptr sim;
-    return simInitAndLoad(filename, fileroot, &sim, flags);
+    return simInitAndLoad(filename.c_str(), fileroot.c_str(), &sim, flags.c_str());
 }
 
 PYBIND11_MODULE(smoldyn, m) {
--- tests/test_py.py
+++ tests/test_py.py
@@ -3,6 +3,8 @@ print("Using smoldyn: %s, %s" % (smoldyn.__file__, smoldyn.__version__))
 
 def test_module():
     print(smoldyn.__version__)
+    print(dir(smoldyn))
+    smoldyn.simulate('../examples/S10_boxes/box.txt', "")
 
 if __name__ == "__main__":
     test_module()
