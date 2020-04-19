3a0968b8cd17cdf9c54dd68374a1b1580a0aef87
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-19 11:45:27 +0530

    double free error.

--- source/py/pysmoldyn.cpp
+++ source/py/pysmoldyn.cpp
@@ -15,6 +15,8 @@ namespace py = pybind11;
 
 int simulate(const string& filepath, const string& flags) 
 {
+    int i, er, pflag, wflag, tflag, Vflag, oflag;
+
     string filename, fileroot;
     auto pos = filepath.find_last_of('/');
 
@@ -23,8 +25,39 @@ int simulate(const string& filepath, const string& flags)
 
     simptr sim;
     cout << "root:" << fileroot << " name:"<<filename << " flags="<< flags<<endl;
-    auto er = simInitAndLoad(fileroot.c_str(), filename.c_str(), &sim, flags.c_str());
-    er=simUpdateAndDisplay(sim); 
+
+    sim = NULL;
+    cout << "root="<< fileroot << " fname=" << filename << ", flags=" << flags 
+        << endl; 
+#ifdef OPTION_VCELL
+    er = simInitAndLoad(fileroot.c_str(), filename.c_str(), &sim, flags.c_str(),
+            new SimpleValueProviderFactory(), new SimpleMesh());
+#else
+    er = simInitAndLoad(fileroot.c_str(), filename.c_str(), &sim, flags.c_str());
+#endif
+    if (!er) {
+        // if (!tflag && sim->graphss && sim->graphss->graphics != 0)
+        //     gl2glutInit(&argc, argv);
+        er = simUpdateAndDisplay(sim);
+    }
+    if (!oflag && !pflag && !er)
+        er = scmdopenfiles((cmdssptr)sim->cmds, wflag);
+    if (pflag || er) {
+        simLog(sim, 4, "%sSimulation skipped\n", er ? "\n" : "");
+    }
+    else {
+        fflush(stdout);
+        fflush(stderr);
+        if (tflag || !sim->graphss || sim->graphss->graphics == 0) {
+            er = smolsimulate(sim);
+            endsimulate(sim, er);
+        }
+        else {
+            smolsimulategl(sim);
+        }
+    }
+    simfree(sim);
+    simfuncfree();
 }
 
 
