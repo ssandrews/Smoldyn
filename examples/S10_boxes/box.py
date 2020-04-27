7864f2d06f55a88f1bf4b8144e58b5a6fbbd4453
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-27 20:00:59 +0530

    now building a static module.

---
+++ examples/S10_boxes/box.py
@@ -0,0 +1,8 @@
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2019-, Dilawar Singh"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+
+import smoldyn
+
+g = smoldyn.Geometry(x=[0,100], y=[0,100], z=[0, 100])
