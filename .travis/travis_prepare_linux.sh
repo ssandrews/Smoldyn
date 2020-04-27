8df3d9922940dc87c7c86ec76580f6dce47a4c89
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-27 21:15:36 +0530

    travis fixes:

--- .travis/travis_prepare_linux.sh
+++ .travis/travis_prepare_linux.sh
@@ -1,21 +1,8 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: travis_prepare_linux.sh
-#
 #         USAGE: ./travis_prepare_linux.sh
-#
 #   DESCRIPTION:  Prepare linux build environment on travis.
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: Always run with sudo permission.
 #        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
 #  ORGANIZATION: NCBS Bangalore
-#       CREATED: 01/02/2017 10:10:02 AM
-#      REVISION:  ---
-#===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 set +e  # Let installation fail in some command
@@ -23,5 +10,5 @@ set +e  # Let installation fail in some command
 apt-get install -qq make cmake
 apt-get install -qq libtiff-dev libzip-dev freeglut3-dev python3-dev 
 apt-get install -qq python3-numpy
-apt-get install -qq python3-setuptools python3-pip
+apt-get install -qq python3-setuptools python3-pip python3-wheel
 apt-get install -qq python3-pytest
--- .travis/travis_prepare_osx.sh
+++ .travis/travis_prepare_osx.sh
@@ -1,21 +1,8 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: travis_prepare_osx.sh
-#
-#         USAGE: ./travis_prepare_osx.sh
-#
 #   DESCRIPTION: Script to prepare OSX build on Travis CI.
 #
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
 #        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
 #  ORGANIZATION: NCBS Bangalore
-#       CREATED: 01/02/2017 10:09:00 AM
-#      REVISION:  ---
-#===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 set +e
@@ -29,5 +16,5 @@ brew install python@3 || echo "Allready installed"
 # To make sure that we do not pick python from /opt etc.
 PATH=/usr/local/bin:/usr/bin:$PATH
 
-python3 -m pip install setuptools --user
+python3 -m pip install setuptools wheel --user
 python3 -m pip install pytest --user
