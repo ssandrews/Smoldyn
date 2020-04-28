0e915301574da257ee84f644160f8a402ce6fd59
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-28 19:01:11 +0000

    enable build and upload to PyPi.

--- docker/test_and_upload.sh
+++ docker/test_and_upload.sh
@@ -20,32 +20,70 @@
 set -e 
 set -x
 set -o nounset                                  # Treat unset variables as an error
-GLOBAL_PIP=/opt/python/cp27-cp27m/bin/pip
-GLOBAL_PY=/opt/python/cp27-cp27m/bin/python
+GLOBAL_PIP=/opt/python/cp37-cp37m/bin/pip
+GLOBAL_PY=/opt/python/cp37-cp37m/bin/python
 $GLOBAL_PIP install twine 
-TWINE=/opt/python/cp27-cp27m/bin/twine
+TWINE=/opt/python/cp37-cp37m/bin/twine
 
 cat <<EOF >/tmp/test.py
-import moose
-import moose.utils as mu
-print( moose.__version__ )
-import rdesigneur as rd
-rdes = rd.rdesigneur()
-rdes.buildModel()
-moose.showfields( rdes.soma )
-moose.reinit( )
-moose.start( 1 )
+import smoldyn
+import smoldyn
+print( smoldy.__version__ )
+
+def test_library():
+    """We test the C API here
+    """
+    s = smoldyn.Smoldyn()
+    s.bounds = ([-50, 50], [-50, 50])
+    assert s.dim == 2
+    s.seed = 1
+    print('Bounds', s.bounds)
+
+    s.setPartitions("molperbox", 4);
+    s.setPartitions("boxsize", 12.5);
+
+    s.addSpecies("ACA")
+    s.addSpecies("ATP")
+    s.addSpecies("cAMP")
+    s.addSpecies("cAR1")
+
+    MS = smoldyn.MS     # enum MolcState
+    SA = smoldyn.SA     # enum SrfAction
+    PF = smoldyn.PF     # enum PanelFace
+    PS = smoldyn.PS     # enum PanelShape
+
+    s.setSpeciesMobility("ACA", MS.all, 1.0)
+    s.setSpeciesMobility("ATP", MS.all, 1.0)
+    s.setSpeciesMobility("cAMP", MS.all, 1.0)
+    s.setSpeciesMobility("cAR1", MS.all, 1.0)
+
+    # Adding surface.
+    s.addSurface("Membrane00")
+    s.setSurfaceAction("Membrane00", PF.both, "ATP", MS.soln, SA.reflect)
+
+    params = [-20.0, 20.0, 10.0, 20.0, 20.0]
+    s.addPanel("Membrane00", PS.sph, "", "", params)
+
+    s.addCompartment("Cell00")
+    s.addCompartmentSurface("Cell00", "Membrane00")
+    s.addCompartmentPoint("Cell00", params)
+    s.addSurfaceMolecules("ACA", MS.down, 30, "Membrane00", PS.all, "all", [])
+    s.addSurfaceMolecules("cAR1", MS.up, 30, "Membrane00", PS.all, "all", [])
+    s.addReaction("r100", "", MS.all, "", MS.all, ["ATP"], [MS.soln], 0.02)
+    s.setReactionRegion("r100", "Cell00", "");
+    s.run(200, dt=0.01, display=False)
+
+def main():
+    test_library()
+
+if __name__ == '__main__':
+    main()
 EOF
 
 for whl in `find $HOME/wheelhouse -name "*.whl"`; do
     echo "Wheel $whl"
     if [[ $whl = *"-py2-"* ]]; then 
-       echo "++ Python2 wheel $whl";
-       PYTHON=/opt/python/cp27-cp27m/bin/python
-       PIP=/opt/python/cp27-cp27m/bin/pip
-       set -e
-       $PIP install $whl
-       $PYTHON /tmp/test.py
+       echo "no py2"
     else
        echo "++ Python3 wheel $whl";
        PYTHON=/opt/python/cp36-cp36m/bin/python
@@ -61,7 +99,7 @@ for whl in $WHEELS; do
     mkdir -p /tmp/wheelhouse
     # If successful, upload using twine.
     if [ -n "$PYPI_PASSWORD" ]; then
-        $TWINE upload $whl --user bhallalab --password $PYPI_PASSWORD --skip-existing
+        $TWINE upload $whl --user dilawar --password $PYPI_PASSWORD --skip-existing
     else
         echo "PYPI password is not set"
         echo "Copying to /tmp/wheelhouse"
