957bf5637b9ec2654ee0639866bdf7bc9886820f
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-28 19:12:18 +0000

    wheels are working fine now.

--- docker/test_and_upload.sh
+++ docker/test_and_upload.sh
@@ -27,13 +27,13 @@ TWINE=/opt/python/cp37-cp37m/bin/twine
 
 cat <<EOF >/tmp/test.py
 import smoldyn
-import smoldyn
-print( smoldy.__version__ )
+print( smoldyn.__version__ )
+from smoldyn import _smoldyn
 
 def test_library():
     """We test the C API here
     """
-    s = smoldyn.Smoldyn()
+    s = _smoldyn.Smoldyn()
     s.bounds = ([-50, 50], [-50, 50])
     assert s.dim == 2
     s.seed = 1
@@ -47,10 +47,10 @@ def test_library():
     s.addSpecies("cAMP")
     s.addSpecies("cAR1")
 
-    MS = smoldyn.MS     # enum MolcState
-    SA = smoldyn.SA     # enum SrfAction
-    PF = smoldyn.PF     # enum PanelFace
-    PS = smoldyn.PS     # enum PanelShape
+    MS = _smoldyn.MS     # enum MolcState
+    SA = _smoldyn.SA     # enum SrfAction
+    PF = _smoldyn.PF     # enum PanelFace
+    PS = _smoldyn.PS     # enum PanelShape
 
     s.setSpeciesMobility("ACA", MS.all, 1.0)
     s.setSpeciesMobility("ATP", MS.all, 1.0)
@@ -86,8 +86,8 @@ for whl in `find $HOME/wheelhouse -name "*.whl"`; do
        echo "no py2"
     else
        echo "++ Python3 wheel $whl";
-       PYTHON=/opt/python/cp36-cp36m/bin/python
-       PIP=/opt/python/cp36-cp36m/bin/pip
+       PYTHON=/opt/python/cp37-cp37m/bin/python
+       PIP=/opt/python/cp37-cp37m/bin/pip
        set -e
        $PIP install $whl
        $PYTHON /tmp/test.py
