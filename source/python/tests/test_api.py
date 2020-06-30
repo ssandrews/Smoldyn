62bf170c81817e4a91a86a0dab9782f69b6baaf0
Dilawar Singh <dilawars@ncbs.res.in>
2020-06-30 16:54:09 +0530

    Tweaks to ctest.
    Run pytest when ctest is executed.
    
    Added a doctest.

--- source/python/tests/test_api.py
+++ source/python/tests/test_api.py
@@ -9,10 +9,17 @@ Description:
 
 import smoldyn as S
 
-
 def test_rectangles():
-    import matplotlib.pyplot as plt
+    """Tests geometric shape by converting them to quivalent smoldyn text
+    lines.
 
+    >>> test_rectangles()
+    panel PanelShape.rect +x 0 10 1 10 20 A
+    panel PanelShape.tri  0 0 0 1 1 1 1 3 -10 t1
+    panel PanelShape.sph  0 -10 0 3 s1
+    panel PanelShape.cyl  20 30 70 20 50 70 20 20 cyl1
+    panel PanelShape.disk  20 20 20 10 10 20 20 d1
+    """
     r1 = S.Rectangle((0, 10, 1), dimensions=(10, 20), axis="+x", name="A")
     print(r1.toText())
     tr = S.Triangle([[0, 0, 0], [1, 1, 1], [1, 3, -10]], name="t1")
@@ -24,10 +31,6 @@ def test_rectangles():
     disk = S.Disk([20, 20, 20], 10, [10, 20, 20], name="d1")
     print(disk.toText())
 
-
-def main():
-    test_rectangles()
-
-
 if __name__ == "__main__":
-    main()
+    import doctest
+    doctest.testmod()
