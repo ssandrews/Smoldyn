c5b7075dba8e082a7e3b458c590ae861d9c1006b
Steven Andrews <steven.s.andrews@gmail.com>
2020-10-16 22:02:04 -0700

    Adding a few more example files.

---
+++ examples/S7_surfaces/lines.py
@@ -0,0 +1,19 @@
+# Creates random lines
+import os
+import random
+
+
+number=input('Enter number of lines: ')
+xlow=input('Enter x low: ')
+xhigh=input('Enter x high: ')
+ylow=input('Enter y low: ')
+yhigh=input('Enter y high: ')
+
+print('start_surface lines')
+for i in range(1,number):
+	x0=random.uniform(xlow,xhigh)
+	x1=random.uniform(xlow,xhigh)
+	y0=random.uniform(ylow,yhigh)
+	y1=random.uniform(ylow,yhigh)
+	print('panel tri %f %f %f %f' %(x0,y0,x1,y1))
+print('end_surface')
