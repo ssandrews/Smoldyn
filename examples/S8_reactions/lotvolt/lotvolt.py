3672c0cfa3e20639cff2534b6a06278a19251be8
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-11-30 19:11:47 +0530

    tweaks to lotvolt script.

--- examples/S8_reactions/lotvolt/lotvolt.py
+++ examples/S8_reactions/lotvolt/lotvolt.py
@@ -15,7 +15,9 @@ fox.addToSolution(1000)
 
 s.setTiff("lotvolt/OpenGl")
 s.setGraphics("opengl", iter=5, text_display=["time", "rabbit", "fox"])
-s.addCommand("molcount lotvoltout.txt", "i", on=0, off=5, step=0.01)
+
+s.setOutputFile("lotvoltout.txt")
+s.addCommand("molcount lotvoltout.txt", "i", on=0, off=20, step=0.01)
 s.addCommand("molcount stdout", "i", on=0, off=20, step=0.1)
 
-s = s.run(20, dt=0.001)
+s = s.run(20, dt=0.001, overwrite=True)
