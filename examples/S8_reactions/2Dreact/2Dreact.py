15a09f3fae3d8fd9bf8339286c66304843bf7e94
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-12-01 10:41:21 +0530

    fixes to tests
    1. The script ./scripts/run_pyscript.py.in now returns the return status
       of underlying test. If a script fail, the test does not call it
       success.  Before this fix, a test would fail only on an exception.
    
    2. Fixes to example scripts. All functions such as `addSphere`,
       `addHemisphere` (any Panel subclass) is removed from the Simulation
       class. Create using smoldyn.Panel or other subclass and then add to
       surface in `addSurface` method.

--- examples/S8_reactions/2Dreact/2Dreact.py
+++ examples/S8_reactions/2Dreact/2Dreact.py
@@ -14,15 +14,9 @@ red = s.addSpecies("red", difc=dict(all=0.1), color=dict(all="red"))
 green = s.addSpecies("green", difc=dict(all=0.1), color=dict(all="green"))
 blue = s.addSpecies("blue", difc=dict(all=0.1), color=dict(all="blue"))
 
-h1 = s.addHemisphere(
-    name="h1", center=[-3, 0, 0], radius=2, vector=[1, 0, 0], slices=10, stacks=5
-)
-h2 = s.addHemisphere(
-    center=[3, 0, 0], radius=2, vector=[-1, 0, 0], slices=10, stacks=5, name="h2"
-)
-c1 = s.addCylinder(
-    start=[-3, 0, 0], end=[3, 0, 0], radius=2, slices=10, stacks=5, name="c1"
-)
+h1 = smoldyn.Hemisphere(name="h1", center=[-3, 0, 0], radius=2, vector=[1, 0, 0], slices=10, stacks=5)
+h2 = smoldyn.Hemisphere(center=[3, 0, 0], radius=2, vector=[-1, 0, 0], slices=10, stacks=5, name="h2")
+c1 = smoldyn.Cylinder(start=[-3, 0, 0], end=[3, 0, 0], radius=2, slices=10, stacks=5, name="c1")
 ecoli = s.addSurface("ecoli", panels=[h1, h2, c1])
 ecoli.setStyle("both", drawmode="edge", color="black")
 
@@ -31,13 +25,9 @@ ecoli.setStyle("both", drawmode="edge", color="black")
 # c1.neighbors = [h1, h2]
 c1.setNeighbors([h1, h2], reciprocal=True)
 
-rxn = s.addReaction(
-    "rxn", subs=[(red, "up"), (green, "up")], prds=[(blue, "up")], rate=0.1
-)
-
+rxn = s.addReaction("rxn", subs=[(red, "up"), (green, "up")], prds=[(blue, "up")], rate=0.1)
 ecoli.addMolecules((red, "up"), 1000, panels=ecoli.panels)
 ecoli.addMolecules((green, "up"), 1000, panels=ecoli.panels)
-
 s.setGraphics("opengl")
 s.addCommand("molcount 2Dreactout.txt", cmd_type="i", on=0, off=100, step=0.1)
 s.run(100, dt=0.01)
