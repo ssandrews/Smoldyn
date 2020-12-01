5c839aaabd463e981376aa98259c21ccc04d1013
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-12-01 11:06:59 +0530

    Fixes to lotvolt: writing to tiff file is fixes
    setTiff() was generating a full path using Path(tiffname).resolve(). The
    C++ core prefix it with sim->filepath making the path invalid. Now setTiff()
    just pass the tiffname to C++ module without doing anything with it. Now
    the behaviour is consistent with lotvolt.txt file.

--- source/python/smoldyn/smoldyn.py
+++ source/python/smoldyn/smoldyn.py
@@ -360,7 +360,7 @@ class Panel(object):
         >>> r1.front.jumpTo(r2.front, True)
         """
         jfrom = getattr(self, face1)
-        assert jfrom, f"Could not locate {face1} on {panel1}."
+        assert jfrom, f"Could not locate {face1} on {self}."
         assert isinstance(jfrom, _PanelFace)
         jto = getattr(panel2, face2)
         assert jto, f"Could not locate {face2} on {panel2}."
@@ -1928,7 +1928,7 @@ class Simulation(object):
 
     def setTiff(
         self,
-        tiffname: Path = Path("OpenGL"),
+        tiffname: str = "OpenGL",
         minsuffix: int = 1,
         maxsuffix: int = 999,
         every: int = 5,
@@ -1937,10 +1937,9 @@ class Simulation(object):
 
         Parameters
         ----------
-        tiffname : str, Path
+        tiffname : str
             Root filename for TIFF files, which may include path information if
-            desired.  If the parent directory does not exists, it will be created.
-            Default is “OpenGL”, which leads to the first TIFF being saved as
+            desired. Default is “OpenGL”, which leads to the first TIFF being saved as
             “OpenGL001.tif”.
         minsuffix : int
             Initial suffix number of TIFF files that are saved.  Default value
@@ -1953,10 +1952,6 @@ class Simulation(object):
             ``every`` is the number of simulation timesteps that should elapse
             between subsequent snapshots.
         """
-        try:
-            Path(tiffname).parent.mkdir(parents=True, exist_ok=True)
-        except Exception as e:
-            __logger__.warning(e)
         k = _smoldyn.setTiffParams(every, str(tiffname), minsuffix, maxsuffix)
         assert k == _smoldyn.ErrorCode.ok
 
