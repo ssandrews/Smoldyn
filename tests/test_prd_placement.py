0d67acaa591b58613d655c70d0a85def7a4ee608
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-12-22 13:35:33 +0530

    Fixes to tests.

--- tests/test_prd_placement.py
+++ tests/test_prd_placement.py
@@ -2,9 +2,11 @@ __author__ = "Dilawar Singh"
 __email__ = "dilawar@subcom.tech"
 
 import smoldyn as S
+import numpy as np
 
 def test_prd_pacement():
     sim = S.Simulation(low=[-10,-10,-10],high=[10,10,10], quit_at_end=1)
+    sim.setRandomSeed(10)
     blue = sim.addSpecies(name='blue',color='blue',difc=1,display_size=0.3)
     red = sim.addSpecies(name='red',color='red',difc=0,display_size=0.3)
     sim.addMolecules(blue,number=100,lowpos=[-5,-5,-5],highpos=[5,5,5])
@@ -16,8 +18,19 @@ def test_prd_pacement():
 
     stick = sim.addReaction(name='stick',subs=[blue,red],prds=[red,red],rate=20)
     stick.productPlacement(method='bounce', param=0.6)
-    sim.setGraphics('opengl_good', 1)
+    sim.addOutputData("data")
+    sim.addCommand("molcount data", cmd_type="E")
+    # sim.setGraphics('opengl_good', 1)
     sim.run(stop=100, dt=0.1)
+    d = sim.getOutputData("data")
+    d = np.array(d)
+    assert (d[:,1] + d[:, 2] == 101).all()
+
+    y = d.std(axis=0)
+    ym = d.mean(axis=0)
+    assert np.allclose(ym, [50, 20.75524, 80.24475], atol=1e-3, rtol=1e-3), ym
+    assert np.allclose(y, [28.89636655, 20.5, 20.5], atol=1e-1, rtol=1e-1), y
+    print(d)
 
 def main():
     test_prd_pacement()
