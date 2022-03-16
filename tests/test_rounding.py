46a183db82f614ac690f5a94df879be86441ea96
Dilawar Singh <dilawar@subcom.tech>
2022-03-16 10:53:12 +0530

    chore: Update pybind11 to 2.9.1

---
+++ tests/test_rounding.py
@@ -0,0 +1,8 @@
+import smoldyn
+
+s = smoldyn.Simulation(
+    low=[-1000, -1000, -10000], high=[1000, 1000, 10000], output_files=["cylinder.txt"]
+)
+
+d1 = s.addDisk(name="d1", center=[50, 25, 8500], radius=850, vector=[0, 0, -1])
+test = s.addSurface("test", panels=[d1])
