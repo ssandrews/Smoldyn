9b437ff52a38401eeced9a92c5b408666b4617e3
Dilawar Singh <dilawar@subcom.tech>
2022-03-17 08:34:32 +0530

    test: Updated test script for #118

--- tests/test_issue_118.py
+++ tests/test_issue_118.py
@@ -4,5 +4,5 @@ s = smoldyn.Simulation(
     low=[-1000, -1000, -10000], high=[1000, 1000, 10000], output_files=["cylinder.txt"]
 )
 
-d1 = s.addDisk(name="d1", center=[50, 25, 8500], radius=850, vector=[0, 0, -1])
+d1 = s.addDisk(name="d1", center=[50, 25, 8500], radius=850, slices=12, vector=[0, 0, -1])
 test = s.addSurface("test", panels=[d1])
