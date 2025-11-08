4782844f4ddb8238c4955f712dd106eeb5598756
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-11-08 15:20:33 +0530

    ci: try using python3.10 again

--- scripts/build_wheels_linux.sh
+++ scripts/build_wheels_linux.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-set -e
-set -x
+
+set -ex
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Place to store wheels.
 WHEELHOUSE=${1-$HOME/wheelhouse}
 echo "Path to store wheels : $WHEELHOUSE"
-rm -rf $WHEELHOUSE && mkdir -p $WHEELHOUSE
+rm -rf "$WHEELHOUSE" && mkdir -p "$WHEELHOUSE"
 
-PYDIR39=/opt/python/cp39-cp39/
 PYDIR310=/opt/python/cp310-cp310/
 PYDIR311=/opt/python/cp311-cp311/
 PYDIR312=/opt/python/cp312-cp312/
 PYDIR313=/opt/python/cp313-cp313/
 
-for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310 $PYDIR39; do
+for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310; do
 	PYTHON=$PYDIR/bin/python
 
 	# dependencies
@@ -25,20 +24,20 @@ for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310 $PYDIR39; do
 	(
 		cd _build_wheel_linux
 		# cmake version must be higher than 3.12
-		PYLIB=$(ls -d $PYDIR/lib/python3.*)
-		PYINDIR=$(ls -d $PYDIR/include/python3.*)
+		PYLIB=$(ls -d "$PYDIR/lib/python3.*")
+		PYINDIR=$(ls -d "$PYDIR/include/python3.*")
 		cmake ../../ \
 			-DOPTION_PYTHON=ON \
 			-DOPTION_EXAMPLES=ON \
 			-DCMAKE_INSTALL_PREFIX=/usr \
-			-DPython3_EXECUTABLE=$PYTHON \
-			-DPython3_INCLUDE_DIR=$PYINDIR \
-			-DPython3_LIBRARY=$PYLIB
+			-DPython3_EXECUTABLE="$PYTHON" \
+			-DPython3_INCLUDE_DIR="$PYINDIR" \
+			-DPython3_LIBRARY="$PYLIB"
 		make -j$(nproc)
 		make wheel
-		export PYTHONPATH=$(pwd)/py
-		$PYTHON -m smoldyn $SCRIPT_DIR/../examples/S4_molecules/mollist.txt
-		$PYTHON -m auditwheel repair *.whl -w $WHEELHOUSE
+		export PYTHONPATH="$(pwd)/py"
+		$PYTHON -m smoldyn "$SCRIPT_DIR/../examples/S4_molecules/mollist.txt"
+		$PYTHON -m auditwheel repair *.whl -w "$WHEELHOUSE"
 
 		# install and test it
 		$PYTHON -m pip install *.whl
@@ -50,16 +49,16 @@ for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310 $PYDIR39; do
 	)
 done
 
-PYTHON=$PYDIR39/bin/python
+PYTHON="${PYDIR310}/bin/python"
 $PYTHON -m pip install twine
 
-ls -lh $WHEELHOUSE/*.whl
+ls -lh "$WHEELHOUSE/*.whl"
 
 # If successful, upload using twine.
 if [ -n "$PYPI_PASSWORD" ]; then
-	$PYTHON -m twine upload $WHEELHOUSE/smoldyn*.whl \
+	$PYTHON -m twine upload "$WHEELHOUSE/smoldyn*.whl" \
 		--user __token__ \
-		--password $PYPI_PASSWORD \
+		--password "$PYPI_PASSWORD" \
 		--skip-existing
 else
 	echo "PYPI password is not set. Not uploading...."
