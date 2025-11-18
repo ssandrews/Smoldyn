eecb51cb8decb157404afe4bac9392abf15cce3c
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-11-18 20:57:29 +0530

    enable 3.14 wheels

--- scripts/build_wheels_linux.sh
+++ scripts/build_wheels_linux.sh
@@ -13,8 +13,9 @@ PYDIR310=/opt/python/cp310-cp310/
 PYDIR311=/opt/python/cp311-cp311/
 PYDIR312=/opt/python/cp312-cp312/
 PYDIR313=/opt/python/cp313-cp313/
+PYDIR314=/opt/python/cp314-cp314/
 
-for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310; do
+for PYDIR in $PYDIR314 $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310; do
 	PYTHON=$PYDIR/bin/python
 
 	# dependencies
@@ -36,6 +37,8 @@ for PYDIR in $PYDIR313 $PYDIR312 $PYDIR311 $PYDIR310; do
 		make wheel
 		export PYTHONPATH="$(pwd)/py"
 		$PYTHON -m smoldyn "$SCRIPT_DIR/../examples/S4_molecules/mollist.txt"
+
+		$PYTHON -m auditwheel show *.whl
 		$PYTHON -m auditwheel repair *.whl -w "$WHEELHOUSE"
 
 		# install and test it
